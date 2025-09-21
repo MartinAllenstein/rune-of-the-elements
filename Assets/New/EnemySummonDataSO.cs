77a17877ee6331a35d780daac07ba81fd4e5e2a9
Martin <martin.allenstein@gmail.com>
2025-09-21 22:10:36 +0700

    Add Stations
    -ClearStation
    -ContainerStation_BasiliskEye

--- Assets/New/EnemySummonDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class EnemySummonDataSO : ScriptableObject
-{
-    public GameObject enemyPrefab;
-    public int enemyID;
-    public string enemyName;
-}
